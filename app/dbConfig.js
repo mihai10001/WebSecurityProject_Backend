9836d45581ae501eb60c86b0df9c807db31108f9
mihai10001 <rvivits@gmail.com>
2021-01-10 15:11:44 +0200

    seed default users for testing

--- app/dbConfig.js
+++ app/dbConfig.js
@@ -8,6 +8,25 @@ const dbClient = new MongoClient(config.dbUri, {
 });
 
 dbClient.connect((error, res) => {
+    // Seed default users for testing purposes
+    // If users already found, don't add them
+    dbClient.db('nodesecurityproject')
+        .collection('users')
+        .findOne({username: 'test'}, (err, user) => {
+            if (!user)
+                dbClient.db('nodesecurityproject').collection('users').insertOne(
+                    {username: 'test', password: 'test', userToEvent: new ObjectID()}
+                );
+        });
+
+    dbClient.db('nodesecurityproject')
+        .collection('users')
+        .findOne({username: 'test2'}, (err, user) => {
+            if (!user)
+                dbClient.db('nodesecurityproject').collection('users').insertOne(
+                    {username: 'test2', password: 'test2', userToEvent: new ObjectID()}
+                );
+        });
 });
 
 module.exports = dbClient;
