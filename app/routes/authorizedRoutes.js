4dcfa4d73f3a6486786b6dfdefd2f98417260966
mihai10001 <rvivits@gmail.com>
2021-01-10 15:25:13 +0200

    implement create-event endpoint, first authorized endpoint

---
+++ app/routes/authorizedRoutes.js
@@ -0,0 +1,32 @@
+const checkTokenMiddleware = require('../checkTokenValidityMiddleware');
+
+module.exports = function(app, dbClient) {
+
+
+    app.post('/create-event', checkTokenMiddleware, (req, res) => {
+        dbClient.db('nodesecurityproject')
+            .collection('users')
+            .findOne({username: req.username}, (err, user) => {
+                if (err)
+                    res.status(400).send('DB error');
+
+                if (user) {
+                    // Information regarding events will be sent by user, eg. startDate
+                    dbClient.db('nodesecurityproject')
+                        .collection('events')
+                        .insertOne({
+                            title: 'random',
+                            description: 'random',
+                            startDate: new Date(),
+                            endDate: new Date(),
+                            allowedUsers: [user.userToEvent]
+                        });
+                } else {
+                    res.status(400).send('User not found');
+                }
+            });
+
+        res.sendStatus(200);
+    });
+
+}
