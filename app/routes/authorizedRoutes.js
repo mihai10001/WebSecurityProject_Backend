45d8503d41e16185f6936e3b029f1ad43a2a4aff
mihai10001 <rvivits@gmail.com>
2021-01-11 18:37:13 +0200

    refactor authorized routes (better return status handling)

--- app/routes/authorizedRoutes.js
+++ app/routes/authorizedRoutes.js
@@ -3,35 +3,31 @@ const checkTokenMiddleware = require('../checkTokenValidityMiddleware');
 module.exports = function(app, dbClient) {
 
     app.get('/events', checkTokenMiddleware, (req, res) => {
-        dbClient.db('nodesecurityproject')
-            .collection('users')
+        dbClient.collection('users')
             .findOne({username: req.username}, (err, user) => {
                 if (err)
-                    res.status(400).send('DB error');
-
-                if (user) {
-                    dbClient.db('nodesecurityproject')
-                        .collection('events')
+                    res.status(500).send('Internal server error: Db error');
+                else if (!user)
+                    res.status(404).send('User not found');
+                else if (user) {
+                    dbClient.collection('events')
                         .find({allowedUsers: user.userToEvent})
                         .toArray()
                         .then(allowedEventsArray => res.json(allowedEventsArray));
-                } else {
-                    res.status(400).send('User not found');
                 }
             });
     });
 
     app.post('/create-event', checkTokenMiddleware, (req, res) => {
-        dbClient.db('nodesecurityproject')
-            .collection('users')
+        dbClient.collection('users')
             .findOne({username: req.username}, (err, user) => {
                 if (err)
-                    res.status(400).send('DB error');
-
-                if (user) {
+                    res.status(500).send('Internal server error: Db error');
+                else if (!user)
+                    res.status(404).send('User not found');
+                else if (user) {
                     // Information regarding events will be sent by user, eg. startDate
-                    dbClient.db('nodesecurityproject')
-                        .collection('events')
+                    dbClient.collection('events')
                         .insertOne({
                             title: 'random',
                             description: 'random',
@@ -39,8 +35,6 @@ module.exports = function(app, dbClient) {
                             endDate: new Date(),
                             allowedUsers: [user.userToEvent]
                         });
-                } else {
-                    res.status(400).send('User not found');
                 }
             });
 
