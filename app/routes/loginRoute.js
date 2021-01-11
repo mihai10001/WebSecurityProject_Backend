d5f90ba32c98da7ff9242aa32886bb9b32066775
mihai10001 <rvivits@gmail.com>
2021-01-11 18:31:56 +0200

    refactor login route (better return status handling), use bcrypt to compare hashes

--- app/routes/loginRoute.js
+++ app/routes/loginRoute.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const secretKey = require('../config').secretKey;
+const bcrypt = require('bcrypt');
 
 module.exports = function(app, dbClient) {
 
@@ -9,18 +10,23 @@ module.exports = function(app, dbClient) {
 
         // This line should require more validation
         if (!username || !password) {
-            res.send('Username or password not given');
+            res.status(400).send('Username or password not given');
         } else {
             // Filter/find first user that meets the criteria from the DB
+            // Compare hashes using bcrypt.compare() method
             // Generate an access token, default signing with HMAC SHA256
-            dbClient.db('nodesecurityproject')
-                .collection('users')
-                .findOne({'username': username,'password': password}, (err, user) => {
-                    if (!user) {
-                        res.send('User not found! Username or password incorrect');
+            dbClient.collection('users')
+                .findOne({'username': username}, (err, user) => {
+                    if (err || !user) {
+                        res.status(404).send('User not found! Username or password incorrect');
                     } else {
-                        const accessToken = jwt.sign({ username: user.username }, secretKey);
-                        res.json({accessToken});  
+                        bcrypt.compare(password, user.password, function(err, result) {
+                            if (err || !result) res.send('Username or password don\'t match');
+                            if (result) {
+                                const accessToken = jwt.sign({ username: user.username }, secretKey);
+                                res.json({accessToken});
+                            }
+                        });
                     }
                 });
         }
