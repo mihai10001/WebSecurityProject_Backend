6fa5edd63c1fc735f0ae491e2c42aed3c73fe2ae
mihai10001 <rvivits@gmail.com>
2021-01-11 18:27:25 +0200

    added config. variable

--- app/config.js
+++ app/config.js
@@ -2,6 +2,8 @@
 // Don't share those! Don't make them public on GitHub, etc.
 const dbUri = '...';
 const secretSigningToken = '...';
+const bcryptSaltRounds = 10;
 
 exports.dbUri = dbUri;
 exports.secretKey = secretSigningToken;
+exports.bcryptSaltRounds = bcryptSaltRounds;
