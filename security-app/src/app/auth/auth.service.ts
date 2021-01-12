68448c9c487c37e777a3453337aff844dd14485a
mihai10001 <rvivits@gmail.com>
2021-01-12 16:49:44 +0200

    implement authentication service with in-memory approach

--- security-app/src/app/auth/auth.service.ts
+++ security-app/src/app/auth/auth.service.ts
@@ -1,22 +1,47 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-private _userIsAuthentificated = false;
+  private _userIsAuthentificated: boolean = false;
+  private _secretToken: string = '';
 
-  get userIsAuthenticated(){
+  get userIsAuthenticated() {
     return this._userIsAuthentificated;
   }
 
-  constructor() { }
+  get token() {
+    return this._secretToken;
+  }
 
-  login(){
-    // this._userIsAuthentificated = true;
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  login(username, password) {
+    this.http
+      .post<any>(environment.apiUrl + '/login', {username: username, password: password})
+      .subscribe(
+        (data) => {
+          if (data) {
+            this._secretToken = data.accessToken as string;
+            this._userIsAuthentificated = true;
+            this.router.navigateByUrl('/events');
+          }
+        },
+        (error) => console.log(error)
+      );
   }
 
   logout(){
-    // this._userIsAuthentificated = false;
+    this._userIsAuthentificated = false;
+    this._secretToken = '';
+    this.router.navigateByUrl('/');
   }
 }
