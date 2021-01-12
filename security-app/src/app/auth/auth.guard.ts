fb7f4ac953667823a36591b2e80c95f64bf46944
mihai10001 <rvivits@gmail.com>
2021-01-12 16:32:05 +0200

    implement basic version of auth guard

--- security-app/src/app/auth/auth.guard.ts
+++ security-app/src/app/auth/auth.guard.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
+import { CanActivate, CanLoad, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router) 
   {}
 
-  canLoad() {
+  canActivate() {
       if(!this.authService.userIsAuthenticated){
         this.router.navigateByUrl('/auth');
       }
       return this.authService.userIsAuthenticated;
-    }
+  }
 
+  canLoad() {
+    if(!this.authService.userIsAuthenticated){
+      this.router.navigateByUrl('/auth');
+    }
+    return this.authService.userIsAuthenticated;
+  }
 }
