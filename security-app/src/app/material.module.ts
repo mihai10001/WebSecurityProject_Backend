f12cfddd8f13b38d227b58f40feabaec5685be8e
mihai10001 <rvivits@gmail.com>
2021-01-18 17:55:34 +0200

    misc. style changes, import missing material modules

--- security-app/src/app/material.module.ts
+++ security-app/src/app/material.module.ts
@@ -6,6 +6,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
@@ -15,7 +19,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatIconModule,
     MatCardModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatProgressBarModule
   ],
   exports: [
     MatButtonModule,
@@ -24,7 +32,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatIconModule,
     MatCardModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatProgressBarModule
   ]
 })
 export class MaterialModule {}
